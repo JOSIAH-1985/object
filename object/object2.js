b849633447b93e26ef220233031d5930f4b31066
JOSIAH-1985 <85343018+JOSIAH-1985@users.noreply.github.com>
2021-07-10 11:35:27 +0100

    Add files via upload

---
+++ object/object2.js
@@ -0,0 +1,17 @@
+const user = {
+    name: 'Josiah',
+    username:'josiah12345',
+    email: 'josiahokworode1@gmail.com',
+    age: 32,
+    blog: ['this my first post', 'hello guys', 'how are you doing'],
+    greet(){
+    console.log('hello josiah ')
+    },
+    logIn(){
+    console.log('you are logged in')
+ },
+     logOut(){
+     console.log('you are logged out')
+ }
+ }
+ user.greet();
